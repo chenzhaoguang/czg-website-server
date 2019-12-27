93ee3af15c0c95617959bc3681e9e63d80143a66
陈昭光 <wb_chenzhaoguang@zhongan.com>
2019-12-27 18:50:57 +0800

    frist commmit

---
+++ src/models/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const fs = require('fs')
+const path = require('path')
+
+const info = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../privateInfo.json')).toString()
+)
+
+const account = info.adminAccount
+const pwd = info.adminPassword
+
+mongoose.connect(
+  // `mongodb://${account}:${pwd}@119.29.145.190:27017/czg666?authSource=admin`
+  `mongodb://localhost:27017/koaBlog`
+)
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'mongodb connection error:'))
+db.once('open', function() {
+  console.log('mongodb connection success!')
+})
