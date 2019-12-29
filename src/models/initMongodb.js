071152c349eddac10c89d88fbeae27edfbb7a849
chenzhaoguang <775982834@qq,com>
2019-12-29 21:42:41 +0800

    feat: mogodb init

--- src/models/initMongodb.js
+++ src/models/initMongodb.js
@@ -11,7 +11,7 @@ const account = info.adminAccount
 const password = info.adminPassword
 
 mongoose.connect(
-  `mongodb://${account}:${pwd}@127.0.0.1:27017/koaBlog?authSource=admin`
+  `mongodb://${account}:${password}@127.0.0.1:27017/koaBlog?authSource=admin`
 )
 
 const db = mongoose.connection
